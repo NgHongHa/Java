4112d9aeb819dd92f1fe32c7de60050f3998b2bb
NgHongHa <ha2k3.dev@gmail.com>
2025-04-15 09:05:14 +0700

    Bai3 kieu du lieu

---
+++ TuHoc/src/Bai4_Kieu_Du_Lieu.java
@@ -0,0 +1,9 @@
+public class Bai4_Kieu_Du_Lieu {
+    public static void main(String[] args) {
+        System.out.println("kiểu nguyên int "+  Integer.MAX_VALUE);
+        System.out.println("kiểu nguyên int " + Integer.MIN_VALUE);System.out.println("kiểu nguyên int "+  Integer.MAX_VALUE);
+        System.out.println("kiểu thuc byte " + Byte.MIN_VALUE);
+        System.out.println("kiểu thuc byte " + Byte.MAX_VALUE);
+
+    }
+}
