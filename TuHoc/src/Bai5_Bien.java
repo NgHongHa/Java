82cef60b88f422b86fbbd6e126c5ead2fe4e2321
NgHongHa <ha2k3.dev@gmail.com>
2025-04-15 09:28:57 +0700

    Bien trong java

---
+++ TuHoc/src/Bai5_Bien.java
@@ -0,0 +1,14 @@
+public class Bai5_Bien {
+    public static <pi> void main(String[] args) {
+        // khai bao bien
+        int a;
+        int b;
+        // khoi tao bien
+        float diem = 9.5f;
+        double diem2 = 9.5;
+        System.out.println(diem2);
+// hang so
+        final double p = 3.14;
+
+    }
+}
