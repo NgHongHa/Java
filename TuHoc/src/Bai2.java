762957061e265e9988c217fa0b0379a7343e8756
NgHongHa <ha2k3.dev@gmail.com>
2025-04-15 08:53:11 +0700

    Bai 2 xuất dữ liệu

---
+++ TuHoc/src/Bai2.java
@@ -0,0 +1,9 @@
+public class Bai2 {
+    public static void main(String[] args) {
+        System.out.println("dòng 1");
+        System.out.print("dòng 2 ko xuống dòngg");
+        System.out.println("\nenter xuống dòng");
+        System.out.println("\t thụt vào");
+        System.out.println("tự học \"java\"");// nháy kép
+    }
+}
