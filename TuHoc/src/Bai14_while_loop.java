d8db697daf27b1bebc3c7d1980817867bcf2cc5a
NgHongHa <ha2k3.dev@gmail.com>
2025-04-15 14:51:24 +0700

    while loop

---
+++ TuHoc/src/Bai14_while_loop.java
@@ -0,0 +1,10 @@
+public class Bai14_while_loop {
+    public static void main(String[] args) {
+        int x = 2;
+        while (x >= 0) {
+            System.out.println("hello" + x);
+            x = --x;
+        }
+    }
+}
+// do while auto chạy lần đầu
