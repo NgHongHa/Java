743368d1f4e729f2d4d500d97eb2c2b58451a011
NgHongHa <ha2k3.dev@gmail.com>
2025-04-15 09:52:37 +0700

    toán tử gán và các phép toán so sánh

---
+++ TuHoc/src/Bai8_toantugan_sosanh.java
@@ -0,0 +1,8 @@
+public class Bai8_toantugan_sosanh {
+    public static void main(String[] args) {
+        int x = 8;
+        x += 5;
+        System.out.println(x);
+        // các phép toasn ss ==, !=,>,<,>=,<=
+    }
+}
