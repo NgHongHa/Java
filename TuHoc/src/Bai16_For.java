b67581c6f9ad370b5847aa83ac9c9464f1d0b3c6
NgHongHa <ha2k3.dev@gmail.com>
2025-04-15 14:51:40 +0700

    for basic

---
+++ TuHoc/src/Bai16_For.java
@@ -0,0 +1,8 @@
+public class Bai16_For {
+    public static void main(String[] args) {
+        // xuất các số chẵn từ 1-100
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("i=" + i);
+        }
+    }
+}
