6f99147968cd2b49dc83f936a65489bfaacb661f
NgHongHa <ha2k3.dev@gmail.com>
2025-04-15 09:35:39 +0700

    ep kieu

---
+++ TuHoc/src/Bai6_epkieu.java
@@ -0,0 +1,8 @@
+public class Bai6_epkieu {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        double c = (double) a / b;
+        System.out.println(c);
+    }
+}
