c2a1a24c0a45420b314dee45bc7a7b35a53e6a8c
NgHongHa <ha2k3.dev@gmail.com>
2025-04-15 08:56:36 +0700

    Bài 3 ghi chú trong java

---
+++ TuHoc/src/Bai3.java
@@ -0,0 +1,10 @@
+public class Bai3 {
+    public static void main(String[] args) {
+       // ghi chú 1 dòng
+       System.out.println("Hello, World!");
+       /*
+       ghi chú nhiều dòng
+       dòng 2
+        */
+    }
+}
