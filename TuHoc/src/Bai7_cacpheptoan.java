16467759a7c95edd40e79c3af19e0efeaba7e75c
NgHongHa <ha2k3.dev@gmail.com>
2025-04-15 09:47:53 +0700

    cac phep toan

---
+++ TuHoc/src/Bai7_cacpheptoan.java
@@ -0,0 +1,13 @@
+public class Bai7_cacpheptoan {
+    public static void main(String[] args) {
+        int a, b;
+        a = 5;
+        b = 10;
+
+        System.out.println(a + b);         // Cộng
+        System.out.println(a - b);         // Trừ
+        System.out.println(a * b);         // Nhân
+        System.out.println((double) a % b);
+        System.out.println((double) a / b); // Chia ra số thực (ép kiểu đng)
+    }
+}
