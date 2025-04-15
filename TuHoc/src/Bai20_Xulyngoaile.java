a954bcfbf7217470829b710a132f4b51f9a8225c
NgHongHa <ha2k3.dev@gmail.com>
2025-04-15 15:30:04 +0700

    try catch

---
+++ TuHoc/src/Bai20_Xulyngoaile.java
@@ -0,0 +1,12 @@
+public class Bai20_Xulyngoaile {
+    public static void main(String[] args) {
+        int a = 10, b = 0;
+
+        try {
+            int c = a / b;
+            System.out.println("code");
+        } catch (Exception e) {
+            System.out.println("loi");
+        }
+    }
+}
